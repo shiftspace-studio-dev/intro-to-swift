469993a558c29b4097448afd99d2a96ed79a0748
ShiftSpace_ Studio <shiftspace_studio@Frengkys-MacBook-Pro.local>
2026-04-16 00:15:07 +0700

    feat(chapter-1): add source code learning material

---
+++ 01-Intro-Swift-and-Setup-Xcode/01-Print-and-Comment.playground/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+
+// Fungsi ini untuk menampilkan pesan
+
+/*
+  fungsi ini untuk menampilkan pesan
+ pesannya adalah "Halo teman-teman"
+ */
+
+print("Halo teman-teman")
+
+
+// Kartu nama digital
+print("------------------------------")
+print("|     KARTU NAMA DIGITAL     |")
+print("------------------------------")
+print("| Nama: ShiftSpace_ Studio   |")
+print("| Kota: Jakarta              |")
+print("| Hobi: Coding & Sharing     |")
+print("------------------------------")
+
+// Daftar Harga
+print("-----------------------------------")
+print("|Nama Produk      Harga      Stok |")
+print("-----------------------------------")
+print("|Apel Malang      Rp.15.000  15kg |")
+print("|Buah Naga        Rp.20.000  20kg |")
+print("|Semangka         Rp.100.000 70kg |")
+print("-----------------------------------")
